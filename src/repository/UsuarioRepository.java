9e4a11fb93a7975bb1d8e8ddd12932c06bcc2f6a
João Vitor Dias <joaovitordiasc.25@gmail.com>
2026-05-08 00:40:41 -0300

    UsuarioRepositoryComApenasSalvar

---
+++ src/repository/UsuarioRepository.java
@@ -0,0 +1,42 @@
+package repository;
+
+import model.Usuario;
+import util.ConnectionFactory;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Date;
+
+public class UsuarioRepository {
+    
+    public void salvar(Usuario usuario) {
+
+        String sql =
+            "INSERT INTO usuario (nome, cpf, email, senha, dataCriacao, ativo) VALUES (?, ?, ?, ?, ?, ?)";
+
+        try (
+            Connection conn = ConnectionFactory.getConnection();
+            PreparedStatement ps = conn.prepareStatement(sql)
+        ) {
+
+            ps.setString(1, usuario.getNome());
+            ps.setString(2, usuario.getCpf());
+            ps.setString(3, usuario.getEmail());
+            ps.setString(4, usuario.getSenha());
+            // ps.setDate(5, new java.sql.Date(usuario.getDataCriacao().getTime()));//Revisar
+            if (usuario.getDataCriacao() != null) {
+                ps.setDate(5, Date.valueOf(usuario.getDataCriacao()));
+            } else {
+                ps.setNull(5, java.sql.Types.DATE);
+            }
+            
+            ps.setBoolean(6, usuario.getAtivo());
+
+            ps.execute();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
